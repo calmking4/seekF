1017057aa84465d0e9b1388a5510dfae7b4a1b73
calmking <2822963337@qq.com>
2026-03-04 16:07:15 +0800

    用户-请求工具类和状态管理

---
+++ seekF-user/app/composables/useApi.js
@@ -0,0 +1,41 @@
+export const useApi = (url, options = {}) => {
+    options.method = options.method || 'POST';
+    const state = useAuthState();
+    return useFetch(url, {
+        baseURL:useRuntimeConfig().public.apiBase,
+        ...options,
+        onRequest:({request,options})=>{
+            const token = state.getToken();
+            if(token){
+                options.headers.set('Authorization', `${token}`)
+            }
+        },
+        onRequestError: (error) => {
+            console.error('onRequestError: ', error);
+        },
+        onResponseError: (error) => {
+            console.error('onResponseError: ', error);
+        },
+    })
+}
+
+export const useApi$ = (url, options = {}) => {
+    options.method = options.method || 'POST';
+    const state = useAuthState();
+    return $fetch(url, {
+        baseURL:useRuntimeConfig().public.apiBase,
+        ...options,
+        onRequest:({request,options})=>{
+            const token = state.getToken();
+            if(token){
+                options.headers.set('Authorization', `${token}`)
+            }
+        },
+        onRequestError: (error) => {
+            console.error('onRequestError: ', error);
+        },
+        onResponseError: (error) => {
+            console.error('onResponseError: ', error);
+        },
+    })
+}
