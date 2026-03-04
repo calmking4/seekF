5f0957a70be5a816fadb2d492840d7d722cc20b8
calmking <2822963337@qq.com>
2026-03-04 17:05:04 +0800

    后端-请求工具类

---
+++ seekF-backend/pkg/resp/response_util.go
@@ -0,0 +1,35 @@
+package resp
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Success(c *gin.Context, message string, data interface{}) {
+	if data != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    200,
+			"message": message,
+			"data":    data,
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    200,
+			"message": message,
+		})
+	}
+}
+
+// Error 错误响应，code 默认值为 400
+func Error(c *gin.Context, message string, code ...int) {
+	respCode := 400 // 默认值
+	if len(code) > 0 {
+		respCode = code[0]
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code":    respCode,
+		"message": message,
+	})
+}
