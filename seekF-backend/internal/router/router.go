e869fd3b2beb8b3a0bb5e0565951f9e714b62815
calmking <2822963337@qq.com>
2026-03-14 17:09:55 +0800

    后端-修改router.go

--- seekF-backend/internal/router/router.go
+++ seekF-backend/internal/router/router.go
@@ -18,45 +18,46 @@ func SetupRouter(
 	// 添加CORS中间件
 	r.Use(middlewares.CORSMiddleware())
 
-	// 不需要认证的公共接口
-	publicGroup := r.Group("/user")
+	// 用户端接口
+	userGroup := r.Group("/user")
 	{
-		publicGroup.POST("/register", authController.Register)
-		publicGroup.POST("/login", authController.Login)
-	}
+		// 无需认证的接口
+		userGroup.POST("/register", authController.Register)
+		userGroup.POST("/login", authController.Login)
 
-	// 需要认证的接口
-	protectedGroup := r.Group("/user")
-	protectedGroup.Use(middlewares.Auth())
-	{
-		protectedGroup.POST("/logout", authController.Logout)
-		//用户信息
-		protectedGroup.POST("/userinfo/getUserinfo", userInfoController.GetUserInfo)
-		protectedGroup.POST("/userinfo/updateUserInfo", userInfoController.UpdateUserInfo)
-		//群组
-		protectedGroup.POST("/group/createGroup", groupController.CreateGroup)
-		protectedGroup.POST("/group/loadMyGroup", groupController.LoadMyGroup)
-		protectedGroup.POST("/group/loadMyJoinedGroup", groupController.LoadMyJoinedGroup)
-		protectedGroup.POST("/group/checkGroupAddMode", groupController.CheckGroupAddMode)
-		protectedGroup.POST("/group/getGroupInfo", groupController.GetGroupInfo)
-		protectedGroup.POST("/group/updateGroupInfo", groupController.UpdateGroupInfo)
-		protectedGroup.POST("/group/getGroupMemberList", groupController.GetGroupMemberList)
-		protectedGroup.POST("/group/removeGroupMembers", groupController.RemoveGroupMembers)
-		protectedGroup.POST("/group/enterGroupDirectly", groupController.EnterGroupDirectly)
-		protectedGroup.POST("/group/leaveGroup", groupController.LeaveGroup)
-		protectedGroup.POST("/group/dismissGroup", groupController.DismissGroup)
-		//联系人
-		protectedGroup.POST("/contact/getUserList", contactController.GetUserList)
-		protectedGroup.POST("/contact/getContactInfo", contactController.GetContactInfo)
-		protectedGroup.POST("/contact/deleteContact", contactController.DeleteContact)
-		protectedGroup.POST("/contact/applyContact", contactController.ApplyContact)
-		protectedGroup.POST("/contact/getNewContactList", contactController.GetNewContactList)
-		protectedGroup.POST("/contact/passContactApply", contactController.PassContactApply)
-		protectedGroup.POST("/contact/blackContact", contactController.BlackContact)
-		protectedGroup.POST("/contact/cancelBlackContact", contactController.CancelBlackContact)
-		protectedGroup.POST("/contact/getApplyGroupList", contactController.GetApplyGroupList)
-		protectedGroup.POST("/contact/refuseContactApply", contactController.RefuseContactApply)
-		protectedGroup.POST("/contact/blackApply", contactController.BlackApply)
+		// 需要认证的接口
+		protectedGroup := userGroup.Group("")
+		protectedGroup.Use(middlewares.Auth())
+		{
+			protectedGroup.POST("/logout", authController.Logout)
+			// 用户信息
+			protectedGroup.POST("/userinfo/getUserinfo", userInfoController.GetUserInfo)
+			protectedGroup.POST("/userinfo/updateUserInfo", userInfoController.UpdateUserInfo)
+			// 群组
+			protectedGroup.POST("/group/createGroup", groupController.CreateGroup)
+			protectedGroup.POST("/group/loadMyGroup", groupController.LoadMyGroup)
+			protectedGroup.POST("/group/loadMyJoinedGroup", groupController.LoadMyJoinedGroup)
+			protectedGroup.POST("/group/checkGroupAddMode", groupController.CheckGroupAddMode)
+			protectedGroup.POST("/group/getGroupInfo", groupController.GetGroupInfo)
+			protectedGroup.POST("/group/updateGroupInfo", groupController.UpdateGroupInfo)
+			protectedGroup.POST("/group/getGroupMemberList", groupController.GetGroupMemberList)
+			protectedGroup.POST("/group/removeGroupMembers", groupController.RemoveGroupMembers)
+			protectedGroup.POST("/group/enterGroupDirectly", groupController.EnterGroupDirectly)
+			protectedGroup.POST("/group/leaveGroup", groupController.LeaveGroup)
+			protectedGroup.POST("/group/dismissGroup", groupController.DismissGroup)
+			// 联系人
+			protectedGroup.POST("/contact/getUserList", contactController.GetUserList)
+			protectedGroup.POST("/contact/getContactInfo", contactController.GetContactInfo)
+			protectedGroup.POST("/contact/deleteContact", contactController.DeleteContact)
+			protectedGroup.POST("/contact/applyContact", contactController.ApplyContact)
+			protectedGroup.POST("/contact/getNewContactList", contactController.GetNewContactList)
+			protectedGroup.POST("/contact/passContactApply", contactController.PassContactApply)
+			protectedGroup.POST("/contact/blackContact", contactController.BlackContact)
+			protectedGroup.POST("/contact/cancelBlackContact", contactController.CancelBlackContact)
+			protectedGroup.POST("/contact/getApplyGroupList", contactController.GetApplyGroupList)
+			protectedGroup.POST("/contact/refuseContactApply", contactController.RefuseContactApply)
+			protectedGroup.POST("/contact/blackApply", contactController.BlackApply)
+		}
 	}
 
 	return r
